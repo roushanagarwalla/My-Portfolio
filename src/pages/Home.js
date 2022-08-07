bc81b18678fc91806be5525a1e779836d1109a2a
Roushan Agarwalla <roshanagarwal375@gmail.com>
2022-08-07 16:08:24 +0530

    added typewriter effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import avatar from "../images/avatar3.png"
+import Typewriter from 'typewriter-effect';
+
 
 export default function Home() {
-  return (
-    <div className='container mt-5'>
-        <div className="row justify-content-center">
-            <div className="col-md-8">
-                <div class="avatar d-flex justify-content-center">
-                    <img src={avatar} className="rounded" width="200px" height="200px" alt="" />
+    return (
+        <div className='container mt-5'>
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="avatar d-flex justify-content-center">
+                        <img src={avatar} className="rounded" width="200px" height="200px" alt="" />
+                    </div>
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h2 className='text-center tert-text'>Hello I'am Roushan Agarwalla</h2>
+                    <h4 className="text-center q-text">
+                        <Typewriter
+                            options={{
+                                strings: ['I am a Web Developer', 'I am a Competitive Programmer', 'I Love to build things'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </h4>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
